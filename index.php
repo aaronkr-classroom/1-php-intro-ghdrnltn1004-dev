69d57531719efd86ac89c38fb59ec1d22d45e712
ghdrnltn1004-dev <ghdrnltn1004@gmail.com>
2026-03-10 16:42:22 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-/* Nothing to see here. */
+$nothing = "<h1 style=' color: red'>Nothing</h1>";
+$to = "<h2 style=' color: orange'>to</h2>";
+$see = "<h3 style=' color: green'>see</h3>";
+$here = "<h4 style=' color: purple'>here.</h4>";
+
+$color = 'blue';
+
+$text = "<h1 style= 'color: $color'>"; 
+$text .= "Nothing to see here."; //.= 결합하는것
+$text .= "</h1> ";
+
+echo $text;
+echo $nothing . ' '. $to .' '. $see .' '. $here;
 ?>
+<!Doctype html>
+<html>
+<head>
+    <title>😁Aron's php page!</title>
+</head>   
+<body>
+    <h1 style="front - size: 48pt">
+        Nothing to see here!
+        <?= $text ?>
+        <h1>
+</body>
+</html>
